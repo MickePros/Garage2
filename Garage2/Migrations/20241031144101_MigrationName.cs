522f6c695ab3bad0b8dd0e5881604da735feca9b
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-10-31 15:42:41 +0100

    DB migration and update

---
+++ Garage2/Migrations/20241031144101_MigrationName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage2.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OverviewViewModel",
+                columns: table => new
+                {
+                    RegNr = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    VehicleType = table.Column<int>(type: "int", nullable: false),
+                    Arrival = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OverviewViewModel", x => x.RegNr);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OverviewViewModel");
+        }
+    }
+}
