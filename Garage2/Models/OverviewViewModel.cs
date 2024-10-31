9b28e8aa6cd0ac27a5c662549ca5b8d8021225f1
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-10-31 12:02:19 +0100

    Added data anotations / changed ParkLenght

--- Garage2/Models/OverviewViewModel.cs
+++ Garage2/Models/OverviewViewModel.cs
@@ -1,16 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Garage2.Models
 {
     public class OverviewViewModel
     {
+        [Display(Name = "Vehicle type")]
         public VehicleType VehicleType { get; set; }
 
         [Key]
+        [Display(Name = "License plate")]
         public string RegNr { get; set; }
 
         public DateTime Arrival { get; set; }
 
-        public DateTime ParkLenght { get; set; }
+        [ReadOnly(true)]
+        [Display(Name = "Time parked")]
+        public TimeSpan ParkLenght => DateTime.Now - Arrival;
     }
 }
