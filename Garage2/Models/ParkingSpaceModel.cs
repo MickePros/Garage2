b6d4f3d6c1fe48aebac2a174e8837906a0fc2600
Ivona Josipovic <josipovic.ivona@gmail.com>
2024-11-01 15:22:26 +0100

    Started work on ParkingSpace model

---
+++ Garage2/Models/ParkingSpaceModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Garage2.Models
+{
+    public class ParkingSpaceModel
+    {
+        private static int Count = 0;
+        public int Id { get; set; }
+
+        public int Number { get; private set; } // Parking slot number to display, auto-incremented and read-only
+
+        public bool IsOccupied { get; set; }
+
+        //Meant for storing liscence plates of vehicles that are parked here - can be more than 1 for motorcycles
+        public List<string> Vehicle { get; private set; } = new List<string>();
+
+        public ParkingSpaceModel()
+        {
+            Number = Count++;
+        }
+    }
+}
